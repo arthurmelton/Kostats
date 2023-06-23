6382e7d04656148c6587e862a8023f47f073ae6a
Arthur Melton <me@arthurmelton.com>
2023-06-23 08:46:12 -0400

    Get player stats better

--- kostats-core/src/realtime/player.rs
+++ kostats-core/src/realtime/player.rs
@@ -1,5 +1,6 @@
 use crate::realtime::{to_int, Command, Item, Realtime};
 use crate::shared::player::Player;
+use serde_json::Value;
 
 impl Realtime {
     pub async fn player_update(&self, message: Command) -> Option<Item> {
@@ -36,59 +37,10 @@ impl Realtime {
                 for i in message.value["users"].as_array()? {
                     if i["user_id"]["velan"].as_i64()? == player_number {
                         let pairs = &i["pairs"];
-                        let game = Box::new(Player {
-                            name: player.clone(),
-
-                            distance_glide: to_int(pairs["lt.distance_glide"].clone()),
-                            distance_sprint: to_int(pairs["lt.distance_walk"].clone()),
-                            distance_walk: to_int(pairs["lt.distance_walk"].clone()),
-                            distance_ballform: to_int(pairs["lt.distance_ballform"].clone()),
-
-                            hit_given: to_int(pairs["lt.unr.hit_given"].clone()),
-                            hit_received: to_int(pairs["lt.unr.hit_received"].clone()),
-
-                            ko_given: to_int(pairs["lt.unr.ko_given"].clone()),
-                            ko_given_doubles: to_int(pairs["lt.unr.ko_given_doubles"].clone()),
-                            ko_given_frenzes: to_int(pairs["lt.unr.ko_given_frenzes"].clone()),
-                            ko_given_triples: to_int(pairs["lt.unr.ko_given_triples"].clone()),
-                            ko_received: to_int(pairs["lt.unr.ko_received"].clone()),
-
-                            successful_tackles: to_int(pairs["lt.unr.successful_tackles"].clone()),
-
-                            playtime: to_int(pairs["lt.tko.playtime"].clone()),
-
-                            mvp: to_int(pairs["lt.tko.match_mvp"].clone()),
-                            match_wins: to_int(pairs["lt.tko.wins_match"].clone()),
-                            rounds_win: to_int(pairs["lt.tko.wins_round"].clone()),
-                            rounds_lost: to_int(pairs["lt.tko.losses_round"].clone()),
+                        return Some(Item::MatchResult {
+                            game: get_player(player.clone(), pairs, "lt"),
+                            total: get_player(player.clone(), pairs, "sn9"),
                         });
-                        let total = Box::new(Player {
-                            name: player,
-
-                            distance_glide: to_int(pairs["sn9.distance_glide"].clone()),
-                            distance_sprint: to_int(pairs["sn9.distance_walk"].clone()),
-                            distance_walk: to_int(pairs["sn9.distance_walk"].clone()),
-                            distance_ballform: to_int(pairs["sn9.distance_ballform"].clone()),
-
-                            hit_given: to_int(pairs["sn9.unr.hit_given"].clone()),
-                            hit_received: to_int(pairs["sn9.unr.hit_received"].clone()),
-
-                            ko_given: to_int(pairs["sn9.unr.ko_given"].clone()),
-                            ko_given_doubles: to_int(pairs["sn9.unr.ko_given_doubles"].clone()),
-                            ko_given_frenzes: to_int(pairs["sn9.unr.ko_given_frenzes"].clone()),
-                            ko_given_triples: to_int(pairs["sn9.unr.ko_given_triples"].clone()),
-                            ko_received: to_int(pairs["sn9.unr.ko_received"].clone()),
-
-                            successful_tackles: to_int(pairs["sn9.unr.successful_tackles"].clone()),
-
-                            playtime: to_int(pairs["sn9.tko.playtime"].clone()),
-
-                            mvp: to_int(pairs["sn9.tko.match_mvp"].clone()),
-                            match_wins: to_int(pairs["sn9.tko.wins_match"].clone()),
-                            rounds_win: to_int(pairs["sn9.tko.wins_round"].clone()),
-                            rounds_lost: to_int(pairs["sn9.tko.losses_round"].clone()),
-                        });
-                        return Some(Item::MatchResult { game, total });
                     }
                 }
                 None
@@ -96,4 +48,33 @@ impl Realtime {
             true => None,
         }
     }
+}
+
+fn get_player(name: String, pairs: &Value, prefix: &str) -> Box<Player> {
+    Box::new(Player {
+        name: name,
+
+        distance_glide: to_int(pairs[format!("{prefix}.distance_glide")].clone()),
+        distance_sprint: to_int(pairs[format!("{prefix}.distance_walk")].clone()),
+        distance_walk: to_int(pairs[format!("{prefix}.distance_walk")].clone()),
+        distance_ballform: to_int(pairs[format!("{prefix}.distance_ballform")].clone()),
+
+        hit_given: to_int(pairs[format!("{prefix}.unr.hit_given")].clone()),
+        hit_received: to_int(pairs[format!("{prefix}.unr.hit_received")].clone()),
+
+        ko_given: to_int(pairs[format!("{prefix}.unr.ko_given")].clone()),
+        ko_given_doubles: to_int(pairs[format!("{prefix}.unr.ko_given_doubles")].clone()),
+        ko_given_frenzes: to_int(pairs[format!("{prefix}.unr.ko_given_frenzes")].clone()),
+        ko_given_triples: to_int(pairs[format!("{prefix}.unr.ko_given_triples")].clone()),
+        ko_received: to_int(pairs[format!("{prefix}.unr.ko_received")].clone()),
+
+        successful_tackles: to_int(pairs[format!("{prefix}.unr.successful_tackles")].clone()),
+
+        playtime: to_int(pairs[format!("{prefix}.tko.playtime")].clone()),
+
+        mvp: to_int(pairs[format!("{prefix}.tko.match_mvp")].clone()),
+        match_wins: to_int(pairs[format!("{prefix}.tko.wins_match")].clone()),
+        rounds_win: to_int(pairs[format!("{prefix}.tko.wins_round")].clone()),
+        rounds_lost: to_int(pairs[format!("{prefix}.tko.losses_round")].clone()),
+    })
 }
